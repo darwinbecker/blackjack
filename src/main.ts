fa49270bd99c20baa9e7637167d59c95ebb1fd80
Darwin Becker <darwinbecker@web.de>
2022-11-24 01:14:31 +0100

    added quasar & pinia

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import vuetify from "./plugins/vuetify";
-import { loadFonts } from "./plugins/webfontloader";
+import { Quasar } from "quasar";
+import { createPinia } from "pinia";
+import "@quasar/extras/material-icons/material-icons.css"; // Import icon libraries
+import "quasar/src/css/index.sass"; // Import Quasar css
 
-loadFonts();
+const blackjackApp = createApp(App);
+const pinia = createPinia();
 
-createApp(App).use(vuetify).mount("#app");
+blackjackApp.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+});
+blackjackApp.use(pinia);
+
+blackjackApp.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,23 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-
-// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-import vuetify from 'vite-plugin-vuetify'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-		vue(),
-		vueJsx(),
-		vuetify({ autoImport: true }),
-	],
+    vue({
+      template: { transformAssetUrls },
+    }),
+    vueJsx(),
+    quasar({
+      sassVariables: "src/quasar-variables.sass",
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
